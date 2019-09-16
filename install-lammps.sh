8c274d824a98daed57ba9bcd3581012064d03b07
Anders Lervik <andersle@gmail.com>
2019-09-16 15:49:41 +0200

    Added script for helping with installation of LAMMPS.

---
+++ install-lammps.sh
@@ -0,0 +1,19 @@
+tarfile=lammps-stable.tar.gz
+version=7Aug19
+target_dir=~/opt/lammps/$version
+target_exe=lammps-$version
+tmp_dir=lammps-source-$version
+
+mkdir $tmp_dir
+tar xvf $tarfile --directory $tmp_dir
+cd $tmp_dir
+cd lammps-$version
+mkdir build
+cd build
+rm -rf *
+
+cmake -D BUILD_MPI=on -D CMAKE_INSTALL_PREFIX=$target_dir -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_Fortran_COMPILER=flang -D PKG_KSPCACE=YES -D PKG_MANYBODY=YES -D PKG_MOLECULE=YES -D PKG_RIGID=YES -D PKG_USER-TALLY=YES ../cmake
+
+make -j4
+
+cp lmp ~/bin/lmp-$version
