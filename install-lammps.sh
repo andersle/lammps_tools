d0cc8254d378b24702dd23228a4cc58ba2b3d408
Anders Lervik <andersle@gmail.com>
2019-09-17 14:30:26 +0200

    Added settings for different compilers

--- install-lammps.sh
+++ install-lammps.sh
@@ -18,7 +18,20 @@ mkdir -p build
 rm -rf build/*
 cd build
 
-cmake -D BUILD_MPI=on -D CMAKE_INSTALL_PREFIX=$target_dir -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_Fortran_COMPILER=flang -D PKG_KSPCACE=YES -D PKG_MANYBODY=YES -D PKG_MOLECULE=YES -D PKG_RIGID=YES -D PKG_USER-TALLY=YES ../cmake
+# Building with GNU Compilers:
+C_COMPILER=gcc
+CXX_COMPILER=g++
+F_COMPILER=gfortran
+# Building with Intel Compilers:
+C_COMPILER=icc
+CXX_COMPILER=icpc
+F_COMPILER=ifort
+# Building with LLVM/Clang Compilers:
+C_COMPILER=clang
+CXX_COMPILER=clang++
+F_COMPILER=flang
+
+cmake -D BUILD_MPI=on -D CMAKE_INSTALL_PREFIX=$target_dir -D CMAKE_C_COMPILER=$C_COMPILER -D CMAKE_CXX_COMPILER=$CXX_COMPILER -D CMAKE_Fortran_COMPILER=$F_COMPILER -D PKG_KSPCACE=YES -D PKG_MANYBODY=YES -D PKG_MOLECULE=YES -D PKG_RIGID=YES -D PKG_USER-TALLY=YES ../cmake
 
 make -j4
 
