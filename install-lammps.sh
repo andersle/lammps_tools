f8e3e28d455bb17a34ae183195cd23424725e3a7
Anders Lervik <andersle@gmail.com>
2019-09-17 12:06:42 +0200

    Automated the install a bit more.

--- install-lammps.sh
+++ install-lammps.sh
@@ -1,17 +1,22 @@
-wget http://lammps.sandia.gov/tars/lammps-stable.tar.gz
 tarfile=lammps-stable.tar.gz
-version=7Aug19
+wget http://lammps.sandia.gov/tars/$tarfile
+
+tmp_dir=lammps-source
+mkdir -p $tmp_dir
+tar xvf $tarfile --directory $tmp_dir
+
+cd $tmp_dir
+lmp_dir=$(find . -maxdepth 1 -type d -name 'lammps-*' -print -quit)
+version=$(echo $lmp_dir | cut -d '-' -f 2)
+echo $version
+
 target_dir=~/opt/lammps/$version
 target_exe=lammps-$version
-tmp_dir=lammps-source-$version
 
-mkdir $tmp_dir
-tar xvf $tarfile --directory $tmp_dir
-cd $tmp_dir
 cd lammps-$version
-mkdir build
+mkdir -p build
+rm -rf build/*
 cd build
-rm -rf *
 
 cmake -D BUILD_MPI=on -D CMAKE_INSTALL_PREFIX=$target_dir -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_Fortran_COMPILER=flang -D PKG_KSPCACE=YES -D PKG_MANYBODY=YES -D PKG_MOLECULE=YES -D PKG_RIGID=YES -D PKG_USER-TALLY=YES ../cmake
 
