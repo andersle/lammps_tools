bb4605627136279cd06ba4ac83dfaa8e9be96e19
Anders Lervik <andersle@gmail.com>
2019-09-24 10:41:09 +0200

    Updated x/y-labels.

--- average_lammps_profile.py
+++ average_lammps_profile.py
@@ -35,7 +35,7 @@ def read_lammps_profile(filename):
                     if data:
                         yield keys, step, data
                         data = []
-                    step, lines_to_read, _ = [int(i) for i in linesplit]
+                    step, lines_to_read = [int(i) for i in linesplit[:2]]
         if data:
             yield keys, step, data
 
@@ -136,8 +136,8 @@ def plot_all_sets(raw_data, key, color_map_name='viridis'):
         if xpos is None:
             xpos = np.arange(len(datai))
         ax1.plot(xpos, datai, color=colors[i], alpha=0.5)
-    ax1.set_ylabel('Bin no.')
-    ax1.set_xlabel(key)
+    ax1.set_xlabel('Bin no.')
+    ax1.set_ylabel(key)
     fig.tight_layout()
 
 
